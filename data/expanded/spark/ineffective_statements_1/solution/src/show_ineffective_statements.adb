5feecd8f6800164a014d07734b9462870a8e550a
Rowan Walshe <walshe@adacore.com>
2024-11-28 11:22:45 +0000

    Add two samples to the unpacked spark dataset

--- data/expanded/spark/ineffective_statements_1/solution/src/show_ineffective_statements.adb
+++ data/expanded/spark/ineffective_statements_1/solution/src/show_ineffective_statements.adb
@@ -1,11 +1,17 @@
 package body Show_Ineffective_Statements is
 
    procedure Swap1 (X, Y : in out T) is
-      Tmp : T;
+      Tmp : T := X;
    begin
-      Tmp := X;
       X   := Y;
-      Y   := X;
+      Y   := Tmp;
    end Swap1;
+
+   procedure Swap2 (X, Y : in out T) is
+      Tmp : T := Y;
+   begin
+      Y := X;
+      X := Tmp;
+   end Swap2;
 
 end Show_Ineffective_Statements;
--- data/expanded/spark/ineffective_statements_1/solution/src/show_ineffective_statements.ads
+++ data/expanded/spark/ineffective_statements_1/solution/src/show_ineffective_statements.ads
@@ -3,5 +3,6 @@ package Show_Ineffective_Statements is
    type T is new Integer;
 
    procedure Swap1 (X, Y : in out T);
+   procedure Swap2 (X, Y : in out T);
 
 end Show_Ineffective_Statements;
