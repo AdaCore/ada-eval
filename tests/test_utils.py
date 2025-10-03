a7a397f38d4618395c416062a09899608d7cd2ea
Seb M'Caw <mcaw@adacore.com>
2025-10-03 16:49:43 +0100

    Add `test_index_from_line_and_col()`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,11 @@ from pathlib import Path
 
 import pytest
 
-from ada_eval.utils import make_files_relative_to
+from ada_eval.utils import (
+    TextPositionOutOfRangeError,
+    index_from_line_and_col,
+    make_files_relative_to,
+)
 
 
 def test_make_files_relative_to():
@@ -40,3 +44,20 @@ def test_make_files_relative_to_with_empty_list():
     files: list[Path] = []
     expected: list[Path] = []
     assert make_files_relative_to(base_path, files) == expected
+
+
+def test_index_from_line_and_col():
+    text = "abc\ndef\nghi"
+    assert text[index_from_line_and_col(text, 1, 1)] == "a"
+    assert text[index_from_line_and_col(text, 1, 2)] == "b"
+    assert text[index_from_line_and_col(text, 1, 3)] == "c"
+    assert text[index_from_line_and_col(text, 1, 4)] == "\n"
+    assert text[index_from_line_and_col(text, 2, 2)] == "e"
+    assert text[index_from_line_and_col(text, 3, 3)] == "i"
+
+    error_msg = "column 5 is out of range [1, 4]"
+    with pytest.raises(TextPositionOutOfRangeError, match=re.escape(error_msg)):
+        index_from_line_and_col(text, 1, 5)
+    error_msg = "line 4 is out of range [1, 3]"
+    with pytest.raises(TextPositionOutOfRangeError, match=re.escape(error_msg)):
+        index_from_line_and_col(text, 4, 1)
