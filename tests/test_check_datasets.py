6c8621bed2e31f9dd5e2eb46d6d114ff835ae928
Seb M'Caw <mcaw@adacore.com>
2025-11-04 14:17:58 +0000

    Fix log message test

--- tests/test_check_datasets.py
+++ tests/test_check_datasets.py
@@ -236,7 +236,7 @@ def test_check_base_datasets(
     )
     with pytest.raises(PassedBaselineEvaluationError, match=error_msg):
         run_check()
-    baseline_msg = "Checking evaluation baseline ..."
+    baseline_msg = "Checking that a null generation fails at least one eval ..."
     assert_log(caplog, INFO, reeval_msg)
     assert_log(caplog, INFO, baseline_msg)
     assert len(caplog.records) == 2
